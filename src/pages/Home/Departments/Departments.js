534d3ccb1c087e08c22cb02c6f4925039f8ab633
Eshad Dewan <eshaddewan571@gmail.com>
2021-10-18 18:41:51 +0600

    kk

--- src/pages/Home/Departments/Departments.js
+++ src/pages/Home/Departments/Departments.js
@@ -1,4 +1,12 @@
 import React from 'react';
+// import Swiper core and required modules
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import 'swiper/swiper-bundle.css'
+
+import SwiperCore, {
+    Pagination
+} from 'swiper';
 
 const Departments = () => {
     return (
